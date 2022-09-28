4593e87088d726e71d4ea1d43c6927fa4c48741d
amitkroutthedev <akrout455@gmail.com>
2022-09-28 10:08:49 +0530

    to display if the user haven't selected anything

---
+++ src/components/UI/EmptyPage.jsx
@@ -0,0 +1,11 @@
+import styles from "./EmptyPage.module.css";
+
+const EmptyPage = () => {
+  return (
+    <div className={styles.emptyPageContainer}>
+      <p>Allot ranks to your favorite dishes</p>
+    </div>
+  );
+};
+
+export default EmptyPage;
